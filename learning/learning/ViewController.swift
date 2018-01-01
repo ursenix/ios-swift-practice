bd8aaa26fe8c9f320af6fb661a741b5190accd74
Senthil Kumaran <ursenix@gmail.com>
2018-01-01 20:09:04 +0530

    Initial commit

---
+++ learning/learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learning
+//
+//  Created by Senthil Kumaran on 01/01/18.
+//  Copyright © 2018 Senthil Kumaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
